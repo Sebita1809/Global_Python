a1e2dd5bddb28c1c0752948132d1436633d7cb1f
Sebita1809 <sebadiazmoyano@gmail.com>
2024-10-31 21:40:01 -0300

    Editando estos archivos

--- Ejecutable.py
+++ Ejecutable.py
@@ -6,7 +6,8 @@ while True:
     respuesta = int(input("Ingrese la opcion: "))
     if respuesta == 1:
         nombreADN = input("Ingrese un nombre para el ADN que desea registrar:\n").upper()
-        ADN = input("Ahora ingrese el ADN de la siguiente forma: AGATCA,GATICA,CAACAT,... :\n").upper()
+        ADN = input("Ahora ingrese el ADN de la siguiente forma: AGATCA,GATCCA,CAACAT,... :\n").upper()
         ADN_lista = ADN.split(",")
         lista_ADN[nombreADN] = verificar_adn(ADN_lista)
+        lista_ADN = {} if lista_ADN[nombreADN] == None else lista_ADN[nombreADN] == verificar_adn(ADN_lista)
         print(lista_ADN)
--- funciones.py
+++ funciones.py
@@ -9,17 +9,20 @@ def inicializar_menu():
         """)
 
 def verificar_adn(ADN):
-    celulas_nitrogenadas = "ACTGI"
+    celulas_nitrogenadas = "ACTG"
     verificador = 0
-    for i in range(len(ADN)):
-        if len(ADN[i]) < 6 or len(ADN[i]) > 6:
-            print("Ha ingresado mal el ADN, debido a que no ingreso la cantidad correcta de datos")
-            verificador = 1
-            break
-        for letra in ADN[i]:
-            if letra not in celulas_nitrogenadas:
-                print(f"La letra {letra} no corresponde una celula nitrogenada")
+    if len(ADN) != 6:
+        print("El ADN ingresado, no cuenta con el largo correcto")  
+    else:
+        for i in range(len(ADN)):
+            if len(ADN[i]) != 6:
+                print("Ha ingresado mal el ADN, debido a que no ingreso la cantidad correcta de datos")
                 verificador = 1
                 break
-        print("Será redirigido nuevamente al menú") if verificador > 0 else print("Su ADN se ha registrado correctamente")
-        return[] if verificador > 0 else ADN
+            for letra in ADN[i]:
+                if letra not in celulas_nitrogenadas:
+                    print(f"La letra {letra} no corresponde una celula nitrogenada")
+                    verificador = 1
+                    break
+            print("Será redirigido nuevamente al menú") if verificador > 0 else print("Su ADN se ha registrado correctamente")
+            return[] if verificador > 0 else ADN
