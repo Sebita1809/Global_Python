8c18904a2b0b84699b543252e192bd1128f39d40
Sebita1809 <sebadiazmoyano@gmail.com>
2024-10-31 21:05:03 -0300

    Agregamos funciones.py

---
+++ funciones.py
@@ -0,0 +1,25 @@
+def inicializar_menu():
+    print("Ingrese el numero de la operacion que desea realizar")
+    print("""
+            1 - Ingresar ADN
+            2 - Detectar mutante
+            3 - Mutar ADN
+            4 - Sanar ADN
+            5 - Salir
+        """)
+
+def verificar_adn(ADN):
+    celulas_nitrogenadas = "ACTGI"
+    verificador = 0
+    for i in range(len(ADN)):
+        if len(ADN[i]) < 6 or len(ADN[i]) > 6:
+            print("Ha ingresado mal el ADN, debido a que no ingreso la cantidad correcta de datos")
+            verificador = 1
+            break
+        for letra in ADN[i]:
+            if letra not in celulas_nitrogenadas:
+                print(f"La letra {letra} no corresponde una celula nitrogenada")
+                verificador = 1
+                break
+        print("Será redirigido nuevamente al menú") if verificador > 0 else print("Su ADN se ha registrado correctamente")
+        return[] if verificador > 0 else ADN
