bc5adf1b678bfad22d82ebff4fc86da53f9699f0
Sebita1809 <sebadiazmoyano@gmail.com>
2024-11-15 10:04:15 -0300

    editando

--- Clases.py
+++ Clases.py
@@ -37,18 +37,60 @@ class Detector:
                         mutante += 1 if encontrar_repeticiones >= 4 else 0
             columna = []
         return True if mutante > 0 else False
-    
 
     def mutante_diagonal1(ADN):
+
         mutante = 0
-        diagonal = []
-        for columna in range(0, int(len(ADN)/2)):
-            cl = columna
+        diagonales = {}        
+        for cl in range(len(ADN)):
+            diagonal = []
+            for fila in range(len(ADN)):
+                columna = cl + fila
+                if columna < len(ADN):
+                    diagonal.append(ADN[fila][columna])
+                    if len(diagonal) >= (6 - cl) and cl <= 2:
+                        diagonales[cl] = diagonal
+                        ##mutante += verificar_diagonal(diagonal[:6-cl])
+                        diagonal = []
+        numero = 3
+        for fi in range(len(ADN)):
+            diagonal = []
+            for cl in range(len(ADN)):
+                fila = fi + cl
+                if fila < len(ADN):
+                    diagonal.append(ADN[cl][fila])
+                    if len(diagonal) >= (6 - fila) and fila <= 2:
+                        print(diagonal)
+                        diagonales[numero] = diagonal
+                        numero += 1
+                        diagonal = []
+        mutante += Detector.verificador(diagonales)
+
+        return mutante >= 1
+
             # attttt,gaaggg,ccaacc,tttaat,ggggaa,ccccca
 
             # tatttt,ggaggg,cccacc,ttttat,ggggga,cccccc
 
             # ttattt,gggagg,ccccac,ttttta,ggggga,ccccca
+            # tttttt,aggggg,cacccc,ttattt,gggagg,ccccac
+    def verificador (diagonales):
+        print(diagonales)
+        encontrar_mutante = 0
+        for i in range(len(diagonales)):
+            diagonal = diagonales[i]
+            for j in range(0,len(diagonal)):
+                if j == 0:
+                    encontrar_mutante +=1 if diagonal[j] == diagonal[j+1] else 0
+                elif j != 6:
+                    encontrar_mutante +=1 if diagonal[j] == diagonal[j-1] else 0
+        return encontrar_mutante
+"""
+    def mutante_diagonal1(ADN):
+        mutante = 0
+        diagonal = []
+        for columna in range(0, int(len(ADN)/2)):
+            cl = columna    
             for fila in range(0, (len(ADN))):
                 if columna == 0:
                     diagonal.append(ADN[fila][fila])
@@ -82,12 +124,4 @@ class Detector:
                         diagonal = []
                         break
         return True if mutante >= 1 else False
-    
-    def verificador (diagonal):
-        encontrar_mutante = 0
-        for i in range(0,len(diagonal)):
-            if i == 0:
-                encontrar_mutante +=1 if diagonal[i] == diagonal[i+1] else 0
-            elif i != 6:
-                encontrar_mutante +=1 if diagonal[i] == diagonal[i-1] else 0
-        return encontrar_mutante
+"""
